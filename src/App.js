61b54306285a4f8fa9565454dfa7792c8a0c7ce3
root <alexandrou.g@hotmail.com>
2019-12-08 21:20:24 +0000

    Changed Welcome Message and Footer

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deployed on Feature Branch
         </a>
       </header>
     </div>
