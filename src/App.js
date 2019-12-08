46e0a5b98044e74acca29962968ce0d3572dda64
root <alexandrou.g@hotmail.com>
2019-12-08 21:34:47 +0000

    Fixed react test issue

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deployed on Feature Branch
+         learn react
         </a>
       </header>
     </div>
