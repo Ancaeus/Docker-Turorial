535655826e2b6c0d6a9c23ba2da44682946ab276
root <alexandrou.g@hotmail.com>
2019-12-08 21:17:11 +0000

    Changed Welcome Message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-     Hello Fast Motherfucker Too!
+     React on AWS Deployed!
         </p>
         <a
           className="App-link"
